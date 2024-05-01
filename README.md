384e7981d563d19105eeb0ddf487289b413ebacc
Paulo Vieira <paulovieira@gmail.com>
2024-05-01 10:53:31 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,188 @@
 # pg-simple-migrations
 simple migrations for postgresql, without leaving the database
+
+- works with postgres 9.4 or above
+
+when an error occurs, we can inspect
+
+```sql
+select jsonb_array_elements(t.diagnostics) from (
+    select diagnostics
+    from migrations
+    order by id desc
+    limit 1
+) t;
+```
+
+TODO: create a inspection helper 
+
+```sql
+select
+    row_number() over () as rn,
+    id as migration_id,
+    row_number() over (partition by id) as rn_migration,
+    r.*
+from migrations
+join lateral jsonb_to_recordset(diagnostics) as r(
+    ts timestamptz,
+    sql_commands text,
+    description text,
+    success bool,
+    author text,
+    message_text text,
+    returned_sqlstate text
+    --pg_exception_context text
+) on true
+order by id desc, rn_migration desc;
+
+select * from show_diagnostics()
+```
+
+
+select * from show_last()
+
+after each migration is run, get a full inspection of the schema 
+
+pg_dump or with a tool like 
+psql --dbname=... --command="\d xyz"
+atlas
+
+
+find previous migrations:
+
+```
+select * 
+from show_diagnostics()
+where success = 't' and sql_command ~* '^.*(my_table).*$'
+```
+
+helper: for simple cases like the previous, we can use this helper (it's equivalent)
+```
+select * from search_migration('my_table')
+```
+
+it will wrap the given string with '^.*(' and ').*$'
+
+for more complex cases, you can provide the regexp yourself. 
+
+For instance, to search for migrations that look like 'alter table xyz drop column something' (that is, that have 'xyz' somewhere and also 'drop' somewhere after 'xyz'):
+
+```
+select * from search_migration('xyz.*drop')
+```
+
+
+```
+export PGDATABASE=something_wrong (to make sure it has effect)
+export PGUSER=...
+export PGPASSWORD=...
+export PGHOST=...
+export PGPORT=...
+
+pg_dump  \
+    --schema-only  \
+    --schema=public \
+    --table=public.* \
+    --schema=other_schema \
+    --table=other_schema.* \
+    --exclude-table-data=*.* \
+    --file=output.sql
+```
+
+
+
+
+other options for pg_dump that make little or no difference (for some of them, the used options are the default values):
+
+```
+--format=plain
+--encoding=UTF8
+--no-tablespaces
+--no-owner
+--no-privileges
+```
+
+
+
+
+NOTE: there 2 ways to create the dump in "schema-only" mode, for inspection: with or without the "--table" option
+
+pg_dump  \
+    --schema=schema_name \
+    --schema-only  \
+    --file=output1.sql
+
+
+pg_dump  \
+    --schema=schema_name \
+    --schema-only  \
+    --table=schema_name.* \
+    --file=output8c.sql
+
+The difference is: 
+- with the "--table" option, it will output only tables (not functions, views, enums, etc)
+- without the "--table" option, it will output the definition of other objects beside tables
+
+If the schema is crowded with lots of functions (from some extension, like postgis, which will install the functions in the public schema), we might want to use "--table=schema_name.*" to output only the tables; but then we loose some of the other objects, like enums and views; it might make sense to do that and create an external script which would extract the enums from the other dump; for views and functions there is not much interest in being part of the dump; we can always see the latest definitions of those in their files (they are "repeatble" migrations);
+
+It might also make sense to have separate dumps for individual tables; the name of the tables would be obtained dynamically, and pg_dump would be called for each table (?if the table uses the enum, would be enum be present?)
+
+TO extract the enums using the full dump (without the "--table" option): call pg_dump without the "--file" option (so that it will output to stdout); use a regular expression that will extracct text between this pattern: 
+
+```
+CREATE TYPE public.chart_type2 AS ENUM (
+    'lines',
+    'bars',
+    'radar',
+    'gauge'
+);
+```
+
+
+NOTE: data to test different options in pg_dump:
+
+```
+create schema migrations
+create table migrations.abc(name text);
+alter table migrations.abc add column user_id int references public.t_users(id);
+
+create schema migrations2
+create table migrations2.abc(name text);
+alter table migrations2.abc add column user_id int references public.t_users(id);
+create table migrations2.abc2(name text, user_id int references public.t_users(id));
+alter table migrations2.abc2 add column something_else text not null;
+insert into migrations2.abc2(name, something_else) values('xx2', 'yy2');
+create type migrations2.mood as enum('x','y')
+ALTER TYPE migrations2.mood ADD VALUE 'orange';
+CREATE OR REPLACE FUNCTION migrations2.test()
+RETURNS void
+AS $$
+BEGIN
+
+    raise notice 'hello world';
+
+END;
+$$ language plpgsql;
+```
+
+
+
+NOTE: in both cases, we could have replaced "--schema-only" with "--exclude-table-data=schema_name.*"; the output would be the same;
+
+
+
+
+IDEIAS:
+create a schema just for functions
+```
+select * from fn.my_function()
+```
+this would be useful for
+-distinguish between our user functions and other functions (built-in)
+-for calling pg_dump with only the schema
+
+
+MORE REFRENECES:
+-https://github.com/sqlalchemy/alembic
+-https://github.com/olirice/pg_migrate
+-https://github.com/sqlalchemy/alembic
--- sketch.sql
+++ sketch.sql
@@ -1,187 +1,274 @@
-drop table migrations;
+drop table migrations cascade;
 create table migrations(
     id int unique not null,
     description text,
-    status text not null CHECK (status in ('success', 'error')),
+    --execution_status text not null CHECK (execution_status in ('success', 'error')),
+    success bool not null,
     author text,
-    diagnostics jsonb,
-    extra jsonb,
-    created_at timestamptz not null,
-    updated_at timestamptz,
-    sql_commands text not null,
-    version text not null
+    diagnostics jsonb default '[]',
+    extra jsonb default '{}',
+    created_at timestamptz not null
+    --updated_at timestamptz,
+    --sql_commands text not null,
+    --version text not null
 );
 
 insert into migrations(
     id,
     description,
-    status,
-    author,
-    created_at,
-    sql_commands,
-    version
+    success,
+    created_at
 )
 values (
     0,
     'initial empty migration',
-    'success',
-    null,
-    now(),
-    '',
-    '0.1'
+    't',
+    now()
 );
 
-
+drop table xyz;
+drop type mood cascade;
+drop type mood3 cascade;
+drop type mood4 cascade;
+drop type mood5 cascade;
+select * from xyz
 select * from migrations;
-select * from migrate('001', $$alter table xyz add column col50 text;$$,'desc 001');
-select * from migrate('002', $$alter table xyz add column col50 text;$$,'desc 002 - will fail');
-select * from migrate('002', $$alter table xyz add column col51 text;$$,'desc 002 - edited');
-select * from migrate('003', $$CREATE TYPE mood AS ENUM ('sad', 'ok', 'happy'); alter table xyz add column col52 mood;$$,'desc 003 - will fail');
-select * from migrate('004', $$alter table xyz drop column col51;$$,'desc 004');
+
+create table xyz()
+select * from migrate('001', $$alter table xyz add column col290 text;$$,'desc 001', 'author1');
+select * from migrate('002', $$alter table xyz add column col290 text;$$,'desc 002 - will fail', 'author1');
+select * from migrate('002', $$alter table xyz add column col291 text;$$,'desc 002 - edited', 'author2');
+select * from migrate('003', $$CREATE TYPE mood AS ENUM ('sad', 'ok', 'happy'); alter table xyz add column col292 mood;$$,'desc 003 - will fail', 'author1');
+select * from migrate('004', $$alter table xyz drop column col291;$$,'desc 004', 'author1');
+select * from migrate('005', $$alter  table xyz drop column col292;$$,'desc 005', 'author1');
+select * from migrate('006', $$alter  table xyz drop column col290;$$,'desc 006', 'author1');
+select * from migrate(7, $$  CREATE TYPE mood3 AS ENUM ('sad', 'ok', 'happy', 'depressed');$$,'desc 007', 'author2');
+select * from migrate('08', $$  CREATE TYPE mood4 AS ENUM ('sad', 'ok', 'happy');$$,'desc 008', 'author1');
+select * from migrate(9, $$  CREATE TYPE mood5 AS ENUM ('sad', 'ok', 'happy');$$,'desc 009', 'author1');
+
+
+
+
+create view diagnostics as
+select t.*
+from (
+           select
+        id as migration_id, row_number() over (partition by id) as rn_migration,
+                            r.* from migrations
+    join lateral jsonb_to_recordset(diagnostics) as r(
+        ts timestamptz,
+        sql_commands text,
+        description text,
+        success bool,
+        author text,
+        message_text text,
+        returned_sqlstate text
+        --pg_exception_context text
+    ) on true
+    order by id desc, rn_migration desc
+) t;
+
+select * from diagnostics
+--where success = 't' and sql_commands ~* '^.*(drop|xyz).*$'
+where sql_commands ~* '^alter.*xyz.*281'
+
+
+
+
+
+
+DROP FUNCTION diagnostics(TEXT);
+CREATE OR REPLACE FUNCTION diagnostics(search_pattern TEXT = '')
+RETURNS SETOF diagnostics
+AS $$
+DECLARE
+    diagnostics_query TEXT;
+BEGIN
+
+    search_pattern = btrim(search_pattern);
+
+    diagnostics_query = 'select * from diagnostics where success = true';
+
+    if search_pattern <> '' then
+        diagnostics_query = diagnostics_query || format(' and sql_commands ~* ''%s'' ', search_pattern);
+    end if;
+
+    --raise notice 'diagnostics_query: %', diagnostics_query;
+
+    RETURN QUERY EXECUTE diagnostics_query;
+
+END;
+$$ language plpgsql;
+
+select * from diagnostics()
+select * from diagnostics('mood3.*enum')
+
 
 
 DROP FUNCTION migrate(TEXT, TEXT, TEXT, TEXT);
-
-CREATE OR REPLACE FUNCTION migrate(migration_id TEXT, _sql_commands TEXT, migration_description TEXT default null, migration_author TEXT default null, OUT execution_code TEXT, OUT execution_description TEXT, OUT execution_diagnostics TEXT)
+CREATE OR REPLACE FUNCTION migrate(migration_id TEXT, sql_commands TEXT, migration_description TEXT default null, migration_author TEXT default null, OUT migrate_code TEXT, OUT execution_diagnostics TEXT)
 RETURNS record
 AS $$
 DECLARE
+    --_version TEXT = '0.1';
+    previous_success_migration record;
+    expected_migration_id int;
+    diagnostics_obj JSONB;
     _message_text TEXT;
-    _pg_exception_context TEXT;
     _returned_sqlstate TEXT;
-    _pg_exception_detail TEXT;
-    _pg_exception_hint TEXT;
-    diagnostics_obj JSON;
-    _version TEXT = '0.1';
-    previous_migration record;
+    -- _pg_exception_context TEXT;
+    -- _pg_exception_detail TEXT;
+    -- _pg_exception_hint TEXT;
 BEGIN
 
     select *
     from migrations
+    where success = 't'
     order by id desc
     limit 1
-    INTO STRICT previous_migration;
+    INTO STRICT previous_success_migration;
 
-    -- early return, case 1: if the previous recorded migration had errors, those errors
-    -- must be corrected (that migration must be successful)
+    -- cases where we do an early return
 
-    if previous_migration.status = 'error' and migration_id::int <> previous_migration.id then
-        execution_code = 'error';
-        execution_description = format('migration %s can''t proceed because the previous recorded migration (%s) has errors', migration_id::int, previous_migration.id);
+    expected_migration_id = previous_success_migration.id + 1;
 
-        raise notice 'migration %: %', migration_id, execution_code;
-        raise notice '%', execution_description;
-
-        return;
+    if migration_id !~ '^\d+$' then
+        migrate_code = 'invalid_migration_id';
+    elseif migration_id::int < expected_migration_id then
+        migrate_code = 'migration_already_done';
+    elseif migration_id::int > expected_migration_id then
+        migrate_code = 'unexpected_migration_id';
     end if;
 
-    -- early return, case 2: migration ids must be consecutive
-
-    if previous_migration.status = 'success' and migration_id::int <> previous_migration.id + 1 then
-        execution_code = 'error';
-        execution_description = format('migration ids must be consecutive (the previous successful recorded migration is %s)', previous_migration.id);
-
-        raise notice 'migration %: %', migration_id, execution_code;
-        raise notice '%', execution_description;
+    if migrate_code is not null then
+        raise notice 'migration %: %', migration_id, migrate_code;
 
         return;
     end if;
 
     -- proceed: try to execute the given sql commands
 
-    execute _sql_commands;
+    sql_commands = btrim(sql_commands);
+    execute sql_commands;
 
-    -- if we arrived here, the sql commands were executed with success
+    -- if we arrive here, the sql commands were executed with success
 
-    insert into migrations(
-        id,
-        description,
-        status,
-        author,
-        created_at,
-        sql_commands,
-        version
-    )
-    values (
-        migration_id::int,
-        migration_description,
-        'success',
-        migration_author,
-        now(),
-        _sql_commands,
-        _version
-    )
-    on conflict (id)
-    DO UPDATE SET
-        description = migration_description,
-        status = 'success',
-        author = migration_author,
-        diagnostics = null,
-        updated_at = now(),
-        sql_commands = _sql_commands,
-        version = _version;
-
-    execution_code = 'success';
-    --execution_description = '';
-
-    raise notice 'migration %: %', migration_id, execution_code;
-
-    return;
-
-EXCEPTION WHEN others THEN
-    GET STACKED DIAGNOSTICS
-        _message_text = MESSAGE_TEXT,
-        _pg_exception_context = PG_EXCEPTION_CONTEXT,
-        _returned_sqlstate = RETURNED_SQLSTATE,
-        _pg_exception_detail = PG_EXCEPTION_DETAIL,
-        _pg_exception_hint = PG_EXCEPTION_HINT;
-
-    diagnostics_obj = json_build_object(
-        'MESSAGE_TEXT', _message_text,
-        'PG_EXCEPTION_CONTEXT', _pg_exception_context,
-        'RETURNED_SQLSTATE', _returned_sqlstate,
-        'PG_EXCEPTION_DETAIL', _pg_exception_detail,
-        'PG_EXCEPTION_HINT', _pg_exception_hint
+    diagnostics_obj = jsonb_build_object(
+        'ts', now(),
+        'sql_commands', sql_commands,
+        'description', migration_description,
+        'success', true,
+        'author', migration_author
     );
 
     insert into migrations(
         id,
         description,
-        status,
+        success,
         author,
         diagnostics,
-        created_at,
-        sql_commands,
-        version
+        created_at
     )
     values (
         migration_id::int,
         migration_description,
-        'error',
+        't',
         migration_author,
-        diagnostics_obj,
-        now(),
-        _sql_commands,
-        _version
+        '[]'::jsonb || diagnostics_obj,
+        now()
     )
     on conflict (id)
     DO UPDATE SET
         description = migration_description,
-        status = 'error',
+        success = 't',
         author = migration_author,
-        diagnostics = diagnostics_obj,
-        updated_at = now(),
-        sql_commands = _sql_commands,
-        version = _version;
+        diagnostics = migrations.diagnostics || diagnostics_obj,
+        created_at = now();
 
-    execution_code = 'error';
-    execution_description = format('the sql commands in migration %s have errors (see diagnostics for more details)', migration_id);
-    execution_diagnostics = diagnostics_obj;
-
-    raise notice 'migration %: %', migration_id, execution_code;
-    raise notice '%', execution_description;
+    migrate_code = 'success';
+    raise notice 'migration %: %', migration_id, migrate_code;
 
     return;
+
+EXCEPTION WHEN others THEN
+    GET STACKED DIAGNOSTICS
+        _message_text = message_text,
+        _returned_sqlstate = returned_sqlstate;
+        -- _pg_exception_context = pg_exception_context;
+        --_pg_exception_detail = pg_exception_detail,
+        --_pg_exception_hint = pg_exception_hint;
+
+    diagnostics_obj = jsonb_build_object(
+        'ts', now(),
+        'sql_commands', sql_commands,
+        'description', migration_description,
+        'success', false,
+        'author', migration_author,
+        'message_text', _message_text,
+        'returned_sqlstate', _returned_sqlstate
+        -- 'pg_exception_context', _pg_exception_context
+        --'pg_exception_detail', _pg_exception_detail,
+        --'pg_exception_hint', _pg_exception_hint
+    );
+
+    insert into migrations(
+        id,
+        description,
+        success,
+        author,
+        diagnostics,
+        created_at
+        --version
+    )
+    values (
+        migration_id::int,
+        migration_description,
+        'f',
+        migration_author,
+        '[]'::jsonb || diagnostics_obj,
+        now()
+        --_version
+    )
+    on conflict (id)
+    DO UPDATE SET
+        description = migration_description,
+        success = 'f',
+        author = migration_author,
+        diagnostics = migrations.diagnostics || diagnostics_obj,
+        created_at = now();
+        --version = _version;
+
+    migrate_code = 'error';
+
+    if _returned_sqlstate = '42601' then
+        migrate_code = 'syntax_error';  -- special case
+    end if;
+
+    execution_diagnostics = diagnostics_obj;
+
+    raise notice 'migration %: %', migration_id, migrate_code;
+
+    return;
+
+END;
+$$ language plpgsql;
+
+-- wrapper function for the case where migration_id is given as an integer (instead of a text)
+
+DROP FUNCTION migrate(INT, TEXT, TEXT, TEXT);
+CREATE OR REPLACE FUNCTION migrate(migration_id INT, sql_commands TEXT, migration_description TEXT default null, migration_author TEXT default null, OUT migrate_code TEXT, OUT execution_diagnostics TEXT)
+RETURNS record
+AS $$
+DECLARE
+    out record;
+BEGIN
+
+    select *
+    from migrate(migration_id::text, sql_commands, migration_description, migration_author)
+    INTO STRICT out;
+
+    migrate_code = out.migrate_code;
+    execution_diagnostics = out.execution_diagnostics;
 
 END;
 $$ language plpgsql;
